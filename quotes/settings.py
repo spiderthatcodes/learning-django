fbbd5d4f27430d4581633de826c2d6038a80c1b6
spiderthatcodes <jessicaldickerson@outlook.com>
2023-08-26 14:23:23 -0700

    functions

--- quotes/settings.py
+++ quotes/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core.apps.CoreConfig',
+    'corsheaders',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +54,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'quotes.urls'
+
+REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES': [   'rest_framework.permissions.AllowAny' ]}
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 TEMPLATES = [
     {
